a2514f847dbd961b0af20979d832243ec9bcf0fb
njt1980 <39239674+njt1980@users.noreply.github.com>
2020-01-21 17:54:08 +0530

    Update

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace webapp4
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Azure!");
             });
         }
     }
